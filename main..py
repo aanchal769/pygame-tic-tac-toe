0e4118db2a6ba3a3ba9ec28c607ca7414ea497d8
Aanchal Gupta <aanchalgupta769@gmail.com>
2026-07-04 21:40:23 +0530

    Store board state and draw X

--- main..py
+++ main..py
@@ -8,11 +8,26 @@ pygame.display.set_caption("Tic Tac Toe")
 
 WHITE=(255,255,255)
 
+board=["","","","","","","","",""]
 
 running=True
 while running:
     screen.fill(WHITE)
 
+    pygame.draw.line(screen,(0,0,0),(225,125),(675,125),5)
+    pygame.draw.line(screen,(0,0,0),(225,275),(675,275),5)
+    pygame.draw.line(screen,(0,0,0),(225,425),(675,425),5)
+    pygame.draw.line(screen,(0,0,0),(225,575),(675,575),5)
+    
+
+    pygame.draw.line(screen,(0,0,0),(225,125),(225,575),5)   
+    pygame.draw.line(screen,(0,0,0),(375,125),(375,575),5)
+    pygame.draw.line(screen,(0,0,0),(525,125),(525,575),5)
+    pygame.draw.line(screen,(0,0,0),(675,125),(675,575),5)
+
+    if(board[0]=="X"):
+        pygame.draw.line(screen,(0,0,0) ,(245,145),(355,255),5)
+        pygame.draw.line(screen,(0,0,0) ,(245,255),(355,145),5)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
@@ -22,24 +37,18 @@ while running:
             x,y=mouse_pos
 
             if 225<=x<=375  and 125<=y<=275:
-                print("Box 1")
+                    board[0]="X"
 
+                    
 
-    pygame.draw.line(screen,(0,0,0),(225,125),(675,125),5)
-    pygame.draw.line(screen,(0,0,0),(225,275),(675,275),5)
-    pygame.draw.line(screen,(0,0,0),(225,425),(675,425),5)
-    pygame.draw.line(screen,(0,0,0),(225,575),(675,575),5)
+   
     
-    pygame.draw.line(screen,(0,0,0),(225,125),(225,575),5)   
-    pygame.draw.line(screen,(0,0,0),(375,125),(375,575),5)
-    pygame.draw.line(screen,(0,0,0),(525,125),(525,575),5)
-    pygame.draw.line(screen,(0,0,0),(675,125),(675,575),5)
+    
 
 
-    pygame.draw.line(screen,(0,0,0) ,(385,290),(510,410),5)
-    pygame.draw.line(screen,(0,0,0) ,(385,410),(510,290),5)
+    
 
-    pygame.draw.circle(screen,(255,0,0),(300,200),65,5)
+    # pygame.draw.circle(screen,(255,0,0),(300,200),65,5)
     pygame.display.update()
 
 pygame.quit()
